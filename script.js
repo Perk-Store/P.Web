cd83cceaecc72423cca1ca1aada0afb537f3021d
Perk-Store <discord.perkdc@gmail.com>
2024-11-10 16:06:54 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,82 @@
+const enterButton = document.getElementById('enterButton');
+const videoContainer = document.getElementById('videoContainer');
+const video = document.querySelector('#videoContainer video');
 
+enterButton.addEventListener('click', () => {
+  enterButton.style.opacity = '0';
+
+  setTimeout(() => {
+    enterButton.style.display = 'none';
+    videoContainer.style.display = 'block';
+
+    setTimeout(() => {
+      videoContainer.style.opacity = '1';
+      video.play();
+    }, 100);
+  }, 500);
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+  const script = document.createElement('script');
+  script.src = 'https://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js';
+  script.onload = function () {
+    particlesJS("snow", {
+      "particles": {
+        "number": {
+          "value": 40,
+          "density": {
+            "enable": true,
+            "value_area": 800
+          }
+        },
+        "color": {
+          "value": "#ffffff"
+        },
+        "opacity": {
+          "value": 0.7,
+          "random": true,
+          "anim": {
+            "enable": true
+          }
+        },
+        "size": {
+          "value": 2.3,
+          "random": true,
+          "anim": {
+            "enable": true
+          }
+        },
+        "line_linked": {
+          "enable": false
+        },
+        "move": {
+          "enable": true,
+          "speed": 5,
+          "direction": "bottom",
+          "random": true,
+          "straight": false,
+          "out_mode": "out",
+          "bounce": false,
+          "attract": {
+            "enable": true,
+            "rotateX": 300,
+            "rotateY": 1200
+          }
+        }
+      },
+      "interactivity": {
+        "events": {
+          "onhover": {
+            "enable": false
+          },
+          "onclick": {
+            "enable": true
+          },
+          "resize": false
+        }
+      },
+      "retina_detect": true
+    });
+  };
+  document.head.append(script);
+});
