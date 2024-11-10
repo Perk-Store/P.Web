0c37ed24f69fb9088316a1fad17da4796b24c603
Perk-Store <discord.perkdc@gmail.com>
2024-11-10 15:32:27 -0500

    Update particles.js

--- js/particles.js
+++ js/particles.js
@@ -1,63 +1,38 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var script = document.createElement('script');
-    script.src = 'https://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js';
+document.addEventListener("DOMContentLoaded", function () {
+    var script = document.createElement("script");
+    script.src = "https://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js";
     script.onload = function () {
         particlesJS("snow", {
-            "particles": {
-                "number": {
-                    "value": 40,
-                    "density": {
-                        "enable": true,
-                        "value_area": 800
+            particles: {
+                number: {
+                    value: 40,
+                    density: {
+                        enable: true,
+                        value_area: 800
                     }
                 },
-                "color": {
-                    "value": "#ffffff"
+                color: { value: "#ffffff" },
+                opacity: {
+                    value: 0.7,
+                    random: true,
+                    anim: { enable: true }
                 },
-                "opacity": {
-                    "value": 0.7,
-                    "random": true,
-                    "anim": {
-                        "enable": true
-                    }
+                size: {
+                    value: 2.3,
+                    random: true,
+                    anim: { enable: true }
                 },
-                "size": {
-                    "value": 2.3,
-                    "random": true,
-                    "anim": {
-                        "enable": true
-                    }
-                },
-                "line_linked": {
-                    "enable": false
-                },
-                "move": {
-                    "enable": true,
-                    "speed": 5,
-                    "direction": "bottom",
-                    "random": true,
-                    "straight": false,
-                    "out_mode": "out",
-                    "bounce": false,
-                    "attract": {
-                        "enable": true,
-                        "rotateX": 300,
-                        "rotateY": 1200
-                    }
+                line_linked: { enable: false },
+                move: {
+                    enable: true,
+                    speed: 5,
+                    direction: "bottom",
+                    random: true,
+                    out_mode: "out"
                 }
             },
-            "interactivity": {
-                "events": {
-                    "onhover": {
-                        "enable": false
-                    },
-                    "onclick": {
-                        "enable": true
-                    },
-                    "resize": false
-                }
-            },
-            "retina_detect": true
+            interactivity: { events: { resize: false } },
+            retina_detect: true
         });
     };
     document.head.append(script);
