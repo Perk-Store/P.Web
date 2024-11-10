0798378ee7238a8abd0e8738ba94884e33f40c01
Perk-Store <discord.perkdc@gmail.com>
2024-11-10 00:13:21 -0500

    Update enter.js

--- js/enter.js
+++ js/enter.js
@@ -1,32 +1,19 @@
 document.querySelector(".enter-button").addEventListener("click", function () {
-    // Hide the enter button and video container, initially hidden
     document.querySelector(".container").style.display = "none";
 
-    // Make the video container visible and play the video
     const videoContainer = document.querySelector(".video-container");
     videoContainer.style.visibility = "visible";
     videoContainer.classList.add("fade-in");
     videoContainer.style.opacity = "1";
 
-    // Show name box
     const nameBox = document.querySelector(".name-box");
     nameBox.style.opacity = "1";
 
-    // Show small buttons (delayed after video and name box fade in)
     const smallButton = document.querySelector(".small-button");
     const secondButton = document.querySelector(".second-button");
     smallButton.style.opacity = "1";
     secondButton.style.opacity = "1";
 
-    // Play the video
     const video = document.getElementById("video");
     video.play();
-});
-
-document.querySelector(".small-button").addEventListener("click", function () {
-    window.open("https://discord.gg/perkdc", "_blank");
-});
-
-document.querySelector(".second-button").addEventListener("click", function () {
-    window.open("https://cash.app/$Kadenru", "_blank");
 });
