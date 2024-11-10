410d0a8d48a7c9348563af0e1f683fca57f7e29f
Perk-Store <discord.perkdc@gmail.com>
2024-11-10 00:15:16 -0500

    Update enter.js

--- js/enter.js
+++ js/enter.js
@@ -1,21 +1,22 @@
 document.querySelector(".enter-button").addEventListener("click", function () {
-    // Hide the main container and display video
+    // Hide the main container
     document.querySelector(".container").style.display = "none";
-
+    
+    // Show the video container and fade it in
     const videoContainer = document.querySelector(".video-container");
     videoContainer.style.visibility = "visible";
     videoContainer.classList.add("fade-in");
     videoContainer.style.opacity = "1";
-
+    
+    // Show the name box and buttons
     const nameBox = document.querySelector(".name-box");
     nameBox.style.opacity = "1";
 
-    // Show the small buttons
     const smallButton = document.querySelector(".small-button");
     const secondButton = document.querySelector(".second-button");
     smallButton.style.opacity = "1";
     secondButton.style.opacity = "1";
-
+    
     // Play the video
     const video = document.getElementById("video");
     video.play();
